01b1d7c2bb8c50e645cfb48758b26f65d75425b5
eduassumpcao <151387203+eduassumpcao@users.noreply.github.com>
2023-12-07 21:15:05 -0300

    create a haversine_distance function

---
+++ foodtrucks/utils/utils.py
@@ -0,0 +1,21 @@
+from math import atan2, cos, radians, sin, sqrt
+
+
+def haversine_distance(lat1, lon1, lat2, lon2):
+    # Convert latitude and longitude from degrees to radians
+    lat1, lon1, lat2, lon2 = map(radians, [lat1, lon1, lat2, lon2])
+
+    # Haversine formula
+    dlat = lat2 - lat1
+    dlon = lon2 - lon1
+
+    a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+    # Radius of the Earth in kilometers (mean value)
+    R = 6371.0
+
+    # Calculate the distance
+    distance = R * c
+
+    return distance
