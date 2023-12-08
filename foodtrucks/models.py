af02a9d36ed99f0112d9bd33e28a365642da219a
eduassumpcao <151387203+eduassumpcao@users.noreply.github.com>
2023-12-07 21:15:58 -0300

    create foodtruck model

--- foodtrucks/models.py
+++ foodtrucks/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class FoodTruck(models.Model):
+    locationid = models.IntegerField(primary_key=True)
+    Applicant = models.TextField()
+    Address = models.TextField()
+    Latitude = models.FloatField()
+    Longitude = models.FloatField()
